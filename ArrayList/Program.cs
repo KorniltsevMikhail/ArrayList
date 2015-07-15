175f9b70135d05ba5f2f4b87f0678621dfaa32bb
korniltsev <korniltsev.mikhail@gmail.com>
2015-07-15 23:52:28 +0300

    init

---
+++ ArrayList/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public class MyArrayList<T> : IEnumerable
+    {
+        T[] data = new T[10];
+        int len = 0;
+
+        public void add(T o)
+        {
+            len++;
+            if (len > data.Length)
+            {
+                T[] newData = new T[(len-1)*2];
+                Array.Copy(data, 0, newData, 0, len - 1);
+                this.data = newData;
+            }
+            this.data[len - 1] = o;
+        }
+        public int size()
+        {
+            return len;
+        }
+        public T get(int index)
+        {
+            if (len == 0 || len <= index || index < 0)
+            {
+                throw new MyOutOfBoundsException();
+            }
+
+            return data[index];
+        }
+        public void set(int index, T o)
+        {
+            if (len == 0 || len <= index)
+            {
+                throw new MyOutOfBoundsException();
+            }
+            data[index] = o;
+
+        }
+        public void remove(int index)
+        {
+            if (len == 0 || len <= index)
+            {
+                throw new MyOutOfBoundsException();
+            }
+            for (int i = index; i < data.Length-1; i++)
+            {
+                data[i] = data[i + 1];
+            }
+            len--;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new MyEnumerator(this);
+        }
+
+        class MyEnumerator : IEnumerator<T>
+        {
+            MyArrayList<T> collection;
+            public MyEnumerator(MyArrayList<T> collection)
+            {
+                this.collection = collection;
+            }
+            int index = -1;
+
+            public T Current
+            {
+                get { return collection.get(index); }
+            }
+
+            object IEnumerator.Current
+            {
+                get { return collection.get(index); }
+            }
+
+            public bool MoveNext()
+            {
+                index++;
+                return index < collection.size();
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+            public void Dispose()
+            {
+                //does nothing
+            }
+
+        }
+
+        
+    }
+    public class MyOutOfBoundsException : Exception
+    {
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArrayList;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void testNewListIsEmpty()
+        {
+            var l = new MyArrayList<string>();
+            Assert.AreEqual(l.size(), 0);
+        }
+
+        [TestMethod]
+        public void testAddOneElementToList()
+        {
+            var l = new MyArrayList<string>();
+            l.add("foo");
+            Assert.AreEqual(l.size(), 1);
+            Assert.AreEqual(l.get(0), "foo");
+        }
+
+        [TestMethod]
+        public void testSetOneElement()
+        {
+            var l = new MyArrayList<string>();
+            l.add("foo");
+            Assert.AreEqual(l.size(), 1);
+            l.set(0, "bar");
+            Assert.AreEqual(l.get(0), "bar");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyOutOfBoundsException))]
+        public void testGetOnEmptyList()
+        {
+            var l = new MyArrayList<string>();
+            l.get(0);           
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyOutOfBoundsException))]
+        public void testGetOnOutOfRange()
+        {
+            var l = new MyArrayList<string>();
+            l.add("str 1");           
+            l.get(1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyOutOfBoundsException))]
+        public void testGetNegative()
+        {
+            var l = new MyArrayList<string>();
+            l.add("str 1");
+            l.get(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyOutOfBoundsException))]
+        public void testSetOnEmptyList()
+        {
+            var l = new MyArrayList<string>();
+            l.set(0, "i'm gona fail mother fucker");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyOutOfBoundsException))]
+        public void testSetOnOutOfRange()
+        {
+            var l = new MyArrayList<string>();
+            l.add("i'll be back");
+            l.set(1, "i'm gona fail mother fucker");
+        }
+
+        [TestMethod]
+        public void testEnumerator() {
+            var l = new MyArrayList<string>();
+            l.add("i'll be back");
+            l.add("i'm gona fail mother fucker");
+
+            var enumerator = l.GetEnumerator();
+
+            Assert.IsTrue(enumerator.MoveNext());
+            Assert.AreEqual(enumerator.Current, "i'll be back");
+            Assert.IsTrue(enumerator.MoveNext());
+            Assert.AreEqual(enumerator.Current, "i'm gona fail mother fucker");
+            Assert.IsFalse(enumerator.MoveNext());
+        }
+
+        [TestMethod]       
+        public void testListExpansion()
+        {
+            var l = new MyArrayList<string>();
+            for (int i = 0; i < 200; i++) {
+                l.add("str#" + i);
+            }
+
+            int index = 0;
+            foreach (var it in l) {
+                Assert.AreEqual(it,"str#" + index);
+                index++;
+            }
+            
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyOutOfBoundsException))]
+        public void testRemoveOnEmpty()
+        {
+            var l = new MyArrayList<string>();
+            l.remove(0);
+            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyOutOfBoundsException))]
+        public void testRemoveNegative()
+        {
+            var l = new MyArrayList<string>();
+            l.remove(-100);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(MyOutOfBoundsException))]
+        public void testRemoveOutOfRange()
+        {
+            var l = new MyArrayList<string>();
+            l.remove(100);
+        }
+
+       
+
+        [TestMethod]
+        public void testRemoveSingle()
+        {
+            var l = new MyArrayList<string>();
+            l.add("foo");
+            l.remove(0);
+            Assert.AreEqual(l.size(), 0);
+        }
+
+
+        [TestMethod]        
+        public void testRemoveInTheMiddleOfTheList()
+        {
+            var l = new MyArrayList<string>();
+            l.add("foo");
+            l.add("bar");
+            l.add("biz");
+            l.remove(1);
+            Assert.AreEqual(l.size(), 2);
+            Assert.AreEqual(l.get(0), "foo");
+            Assert.AreEqual(l.get(1), "biz");
+        }
+        
+
+       
+    }
+}
